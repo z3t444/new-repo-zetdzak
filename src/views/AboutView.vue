c36921227a4691aaa191d9e56d0d84a211e7a1a1
Z3T444HCK <113794999+Zetiml0ck@users.noreply.github.com>
2023-12-22 20:52:09 +0700

    push

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -15,148 +15,43 @@
       <p class="p-8 max_paragraph sm:text-lg mt-10 dm_sans text-center mx-auto text-gray-700 dark:text-gray-300 lg:w-11/12">ZetDzak takes chat and collaboration to the next level: daily standups, team check-ins, surveys, kudos, best companion bot for your virtual watercooler, motivation <span class="light_lamp">💡</span></p>
     </div>
     <!-- // -->
-    <div class="graph_peformance">
-      <div class="for_import">
-        <Graph />
-      </div>
-    </div>
 
-    <section class="ClassName dm_sans mt-60">
-      <div class="container px-6 py-10 mx-auto">
-        <h1 class="text-3xl font-medium Explore_our_leading text-gray-800 capitalize lg:text-4xl dark:text-gray-200">
-          explore our <br />
-          awesome <span class="underline font-semibold decoration-blue-500">Components</span>
-        </h1>
-        <p class="mt-4 text-gray-500 xl:mt-6 dark:text-gray-300">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum quam voluptatibus</p>
-        <div class="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 md:grid-cols-2 xl:grid-cols-3">
-          <div class="p-8 space-y-3 border border-blue-400 dark:border-gray-900 hover:border-blue-400 duration-100 hover:scale-105 rounded-xl">
-            <span class="inline-block text-blue-500 dark:text-blue-400">
-              <svg xmlns="http://www.w3.org/2000/svg" class="w-8 h-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 18.657A8 8 0 016.343 7.343S7 9 9 10c0-2 .5-5 2.986-7C14 5 16.09 5.777 17.656 7.343A7.975 7.975 0 0120 13a7.975 7.975 0 01-2.343 5.657z" />
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.879 16.121A3 3 0 1012.015 11L11 14H9c0 .768.293 1.536.879 2.121z" />
-              </svg>
-            </span>
-
-            <h1 class="text-2xl font-semibold text-gray-700 capitalize dark:text-gray-200">elegant Dark Mode</h1>
-            <p class="text-gray-500 dark:text-gray-300">Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident ab nulla quod dignissimos vel non corrupti doloribus voluptatum eveniet</p>
-            <a href="#" class="inline-flex p-2 text-blue-500 capitalize transition-colors duration-200 transform bg-blue-100 rounded-full dark:bg-blue-500 dark:text-white hover:underline hover:text-blue-600 dark:hover:text-blue-500">
-              <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-              </svg>
-            </a>
-          </div>
-
-          <div class="p-8 space-y-3 border border-blue-400 dark:border-gray-900 hover:border-blue-400 duration-100 hover:scale-105 rounded-xl">
-            <span class="inline-block text-blue-500 dark:text-blue-400">
-              <svg xmlns="http://www.w3.org/2000/svg" class="w-8 h-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 4a2 2 0 114 0v1a1 1 0 001 1h3a1 1 0 011 1v3a1 1 0 01-1 1h-1a2 2 0 100 4h1a1 1 0 011 1v3a1 1 0 01-1 1h-3a1 1 0 01-1-1v-1a2 2 0 10-4 0v1a1 1 0 01-1 1H7a1 1 0 01-1-1v-3a1 1 0 00-1-1H4a2 2 0 110-4h1a1 1 0 001-1V7a1 1 0 011-1h3a1 1 0 001-1V4z" />
-              </svg>
-            </span>
-
-            <h1 class="text-2xl font-semibold text-gray-700 capitalize dark:text-gray-200">Easy to customiztions</h1>
-            <p class="text-gray-500 dark:text-gray-300">Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident ab nulla quod dignissimos vel non corrupti doloribus voluptatum eveniet</p>
-            <a href="#" class="inline-flex p-2 text-blue-500 capitalize transition-colors duration-200 transform bg-blue-100 rounded-full dark:bg-blue-500 dark:text-white hover:underline hover:text-blue-600 dark:hover:text-blue-500">
-              <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-              </svg>
-            </a>
-          </div>
-
-          <div class="p-8 space-y-3 border border-blue-400 dark:border-gray-900 hover:border-blue-400 duration-100 hover:scale-105 rounded-xl">
-            <span class="inline-block text-blue-500 dark:text-blue-400">
-              <svg xmlns="http://www.w3.org/2000/svg" class="w-8 h-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z" />
-              </svg>
-            </span>
-
-            <h1 class="text-2xl font-semibold text-gray-700 capitalize dark:text-gray-200">Simple & clean designs</h1>
-            <p class="text-gray-500 dark:text-gray-300">Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident ab nulla quod dignissimos vel non corrupti doloribus voluptatum eveniet</p>
-            <a href="#" class="inline-flex p-2 text-blue-500 capitalize transition-colors duration-200 transform bg-blue-100 rounded-full dark:bg-blue-500 dark:text-white hover:underline hover:text-blue-600 dark:hover:text-blue-500">
-              <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-              </svg>
-            </a>
-          </div>
-        </div>
-        <div class="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 md:grid-cols-2 xl:grid-cols-3">
-          <div class="p-8 space-y-3 border border-blue-400 dark:border-gray-900 hover:border-blue-400 duration-100 hover:scale-105 rounded-xl">
-            <span class="inline-block text-blue-500 dark:text-blue-400">
-              <svg xmlns="http://www.w3.org/2000/svg" class="w-8 h-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 18.657A8 8 0 016.343 7.343S7 9 9 10c0-2 .5-5 2.986-7C14 5 16.09 5.777 17.656 7.343A7.975 7.975 0 0120 13a7.975 7.975 0 01-2.343 5.657z" />
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.879 16.121A3 3 0 1012.015 11L11 14H9c0 .768.293 1.536.879 2.121z" />
-              </svg>
-            </span>
-
-            <h1 class="text-2xl font-semibold text-gray-700 capitalize dark:text-gray-200">elegant Dark Mode</h1>
-            <p class="text-gray-500 dark:text-gray-300">Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident ab nulla quod dignissimos vel non corrupti doloribus voluptatum eveniet</p>
-            <a href="#" class="inline-flex p-2 text-blue-500 capitalize transition-colors duration-200 transform bg-blue-100 rounded-full dark:bg-blue-500 dark:text-white hover:underline hover:text-blue-600 dark:hover:text-blue-500">
-              <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-              </svg>
-            </a>
-          </div>
-
-          <div class="p-8 space-y-3 border border-blue-400 dark:border-gray-900 hover:border-blue-400 duration-100 hover:scale-105 rounded-xl">
-            <span class="inline-block text-blue-500 dark:text-blue-400">
-              <svg xmlns="http://www.w3.org/2000/svg" class="w-8 h-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 4a2 2 0 114 0v1a1 1 0 001 1h3a1 1 0 011 1v3a1 1 0 01-1 1h-1a2 2 0 100 4h1a1 1 0 011 1v3a1 1 0 01-1 1h-3a1 1 0 01-1-1v-1a2 2 0 10-4 0v1a1 1 0 01-1 1H7a1 1 0 01-1-1v-3a1 1 0 00-1-1H4a2 2 0 110-4h1a1 1 0 001-1V7a1 1 0 011-1h3a1 1 0 001-1V4z" />
-              </svg>
-            </span>
-
-            <h1 class="text-2xl font-semibold text-gray-700 capitalize dark:text-gray-200">Easy to customiztions</h1>
-            <p class="text-gray-500 dark:text-gray-300">Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident ab nulla quod dignissimos vel non corrupti doloribus voluptatum eveniet</p>
-            <a href="#" class="inline-flex p-2 text-blue-500 capitalize transition-colors duration-200 transform bg-blue-100 rounded-full dark:bg-blue-500 dark:text-white hover:underline hover:text-blue-600 dark:hover:text-blue-500">
-              <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-              </svg>
-            </a>
-          </div>
-
-          <div class="p-8 space-y-3 border border-blue-400 dark:border-gray-900 hover:border-blue-400 duration-100 hover:scale-105 rounded-xl">
-            <span class="inline-block text-blue-500 dark:text-blue-400">
-              <svg xmlns="http://www.w3.org/2000/svg" class="w-8 h-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z" />
-              </svg>
-            </span>
-
-            <h1 class="text-2xl font-semibold text-gray-700 capitalize dark:text-gray-200">Simple & clean designs</h1>
-            <p class="text-gray-500 dark:text-gray-300">Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident ab nulla quod dignissimos vel non corrupti doloribus voluptatum eveniet</p>
-            <a href="#" class="inline-flex p-2 text-blue-500 capitalize transition-colors duration-200 transform bg-blue-100 rounded-full dark:bg-blue-500 dark:text-white hover:underline hover:text-blue-600 dark:hover:text-blue-500">
-              <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-              </svg>
-            </a>
-          </div>
-        </div>
-      </div>
-    </section>
-
-    <div class="flex flex-col sm:flex-row mt-40 gap-10">
+    <div class="flex flex-col justify-between items-center sm:flex-row mt-40 gap-10">
       <div class="p-4 w-full sm:w-1/2">
         <p class="text-orange-400">Master of Management System</p>
-        <h3 class="dark:text-gray-200 font-normal mt-5 text-5xl leading-tight">
-          Manage All <span class="system_text_css font-semibold">system</span> and <br />
-          data <span class="security_text_css font-semibold">Security</span>
+        <h3 class="dark:text-gray-200 font-medium mt-2 text-5xl leading-tight">
+          Manage All <span class="system_text_css">system</span> and <br />
+          data <span class="security_text_css">Security</span>
         </h3>
-        <p class="dark:text-gray-200 mt-7 leading-7">
+        <p class="dark:text-gray-200 mt-8 leading-8">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum necessitatibus <br />
           beatae perspiciatis porro cumque tempore, ipsa similique omnis voluptates esse!
         </p>
-        <p class="dark:text-gray-200 mt-5 leading-7">
+        <p class="dark:text-gray-200 mt-7 leading-8">
           Lorem ipsum dolor sit, amet consectetur <br />
           adipisicing elit.
         </p>
-        <div class="flex flex-col sm:flex-row md:flex-col lg:flex-row mt-10 gap-10">
-          <div class="bg-blue-200 p-4 w-full sm:w-1/2 md:w-full lg:w-1/3">Box 1</div>
-          <div class="bg-green-200 p-4 w-full sm:w-1/2 md:w-full lg:w-1/3">Box 2</div>
-          <div class="bg-yellow-200 p-4 w-full sm:w-1/2 md:w-full lg:w-1/3">Box 3</div>
+        <div class="flex items-center flex-col sm:flex-row md:flex-col lg:flex-row mt-10 gap-10">
+          <a href="#" class="bg-blue-400 dark:text-gray-200 hover:bg-transparent hover:border text-black hover:border-blue-400 text-center w-36 duration-100 p-4 rounded-full">Read More</a>
+          <div class="p-4 w-full sm:w-1/2 md:w-full dark:text-gray-200 lg:w-1/3 system_text_css">
+            <h3 class="text-5xl font-medium">78%</h3>
+            <p class="mt-3">Lorem, ipsum dolor.</p>
+          </div>
+          <div class="p-4 w-full sm:w-1/2 md:w-full lg:w-1/3 security_text_css">
+            <h3 class="text-5xl font-medium">80%</h3>
+            <p class="mt-3">Lorem ipsum dolor sit.</p>
+          </div>
         </div>
       </div>
-      <div class="bg-green-200 p-4 w-full sm:w-1/2">Box 2</div>
+      <div class="p-4 w-full sm:w-1/2 rounded-2xl">
+        <div class="for_import">
+          <Graph />
+        </div>
+      </div>
     </div>
 
     <section class="ClassName dm_sans">
-      <div class="container px-6 py-10 mx-auto mt-60">
+      <div class="container px-6 py-10 mx-auto mt-40">
         <h1 class="text-2xl font-medium text-center text-gray-800 capitalize lg:text-5xl dark:text-gray-200">gallery For Us Peformance</h1>
 
         <div class="class_grid_image grid grid-cols-1 sm:gap xl:mt-20 xl:gap-12 lg:grid-cols-2">
@@ -200,13 +95,5 @@
 
   .Explore_our_leading {
     line-height: 50px;
-  }
-
-  .system_text_css {
-    text-shadow: 0px 0px 40px rgb(0, 153, 255);
-  }
-
-  .security_text_css {
-    text-shadow: 0px 0px 30px red;
   }
 </style>
