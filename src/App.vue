0bf223a89eaa331fd22c764fcaadfe7e213f2020
Z3T444HCK <ebdtv4@gmail.com>
2024-01-06 21:14:55 +0700

    #Remove (Style Import Font)

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   import Default from "./common/Default.vue"
 </script>
 <template>
-  <html class="html">
+  <html class="html dm_sans">
     <main class="layout">
       <header-import>
         <Default />
@@ -11,7 +11,12 @@
   </html>
 </template>
 <style scoped>
-.layout {
-  background-color: #14141f;
-}
+  .layout {
+    background-color: #14141f;
+  }
+  @import url("https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,300;0,9..40,400;0,9..set40,500;0,9..40,600;0,9..40,700;0,9..40,800;0,9..40,900;0,9..40,1000;1,9..40,300;1,9..40,400;1,9..40,500;1,9..40,600;1,9..40,700;1,9..40,800;1,9..40,900;1,9..40,1000&display=swap");
+ 
+  .dm_sans {
+    font-family: "DM Sans", sans-serif;
+  }
 </style>
--- src/common/Default.vue
+++ src/common/Default.vue
@@ -15,7 +15,7 @@
   }
   import "../assets/css/allert.css"
   import "../assets/css/responsive.css"
-  import '../common/style/defaultCommon.css'
+  import "../common/style/defaultCommon.css"
 </script>
 
 <template>
@@ -29,7 +29,7 @@
         <div class="flex wrapperrast justify-between items-center">
           <a href="#">
             <div class="main-logo flex space-x-3 items-center">
-              <img src="../assets/img/NusantaraDeveloperlogo.jpg" class="rounded-full" width="50px" alt="">
+              <img src="../assets/img/NusantaraDeveloperlogo.jpg" class="rounded-full" width="50px" alt="" />
               <h3 class="text-lg dark:text-white font-semibold">Nusantara Developer</h3>
             </div>
           </a>
@@ -77,12 +77,3 @@
     </div>
   </div>
 </template>
-
-<style scoped>
-  @import url("https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,300;0,9..40,400;0,9..40,500;0,9..40,600;0,9..40,700;0,9..40,800;0,9..40,900;0,9..40,1000;1,9..40,300;1,9..40,400;1,9..40,500;1,9..40,600;1,9..40,700;1,9..40,800;1,9..40,900;1,9..40,1000&display=swap");
-  .dm_sans {
-    font-family: "DM Sans", sans-serif;
-  }
-
-
-</style>
