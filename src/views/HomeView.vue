08215a281464e63675ecc9065808eacfaf6c3b26
Z3T444HCK <ebdtv4@gmail.com>
2024-01-09 01:41:36 +0700

    #initial commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -285,7 +285,7 @@
                 <input type="hidden" name="access_key" value="3a928331-13f9-4e84-86cf-adb4fb41d57e" />
                 <div class="gap-3 flex">
                   <input type="text" name="name" required placeholder="Your Name" class="w-full p-5 rounded-2xl mt-3 shadow-lg focus:outline-none bg-transparent focus:border-blue-500 bg-[#1c1c2e]"/>
-                  <input type="email" name="email" required placeholder="Your Email" class="w-full p-5 rounded-2xl shadow-lg mt-3 bg-transparent focus:border-blue-500" style="background-color: #1c1c2e" />
+                  <input type="email" name="email" required placeholder="Your Email" class="w-full p-5 rounded-2xl shadow-lg mt-3 bg-transparent focus:border-blue-500 bg-[#1c1c2e]" />
                 </div>
                 <textarea name="message" required placeholder="Your Message" class="w-full px-4 py-4 h-60 rounded-2xl shadow-lg focus:outline-none bg-transparent focus:border-blue-500 bg-[#1c1c2e]"></textarea>
                 <button type="submit" class="w-48 mx-auto py-4 px-4 text-sm mt-4 text-white font-semibold rounded-3xl hover:scale-95 hover:text-gray-200 hover:bg-blue-600 focus:outline-none duration-100 focus:bg-blue-600" style="background-color: #1c1c2e">Submit Form</button>
