6edf8bcd385223b94d0bdededfca4543e11e12a4
Z3T444HCK <ebdtv4@gmail.com>
2024-01-09 01:45:56 +0700

    #update (About Page)

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -70,7 +70,7 @@
           <div class="w-full md:w-1/2 p-4 margin_state">
             <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
               <div class="grid grid-cols-1 gap-6">
-                <div class="p-6 rounded-3xl flex space-x-3 dark:border dark:border-gray-800 hover:scale-105 duration-100" style="background-color: #1c1c2e">
+                <div class="p-6 rounded-3xl flex space-x-3 dark:border dark:border-gray-800 hover:scale-105 duration-100 bg-[#1c1c2e]">
                   <Discord />
                   <div>
                     <h4 class="dark:text-gray-300">View Of</h4>
