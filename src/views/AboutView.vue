9c0fc92d8187481e5b6d9a9b0ee7532d86af6fe0
Z3T444HCK <ebdtv4@gmail.com>
2024-01-09 01:46:13 +0700

    #Update (About PAge)

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -77,7 +77,7 @@
                     <h4 class="dark:text-gray-200 font-medium text-lg mt-2">Discord Server</h4>
                   </div>
                 </div>
-                <div class="p-6 rounded-3xl flex space-x-3 dark:border dark:border-gray-800 hover:scale-105 duration-100" style="background-color: #1c1c2e">
+                <div class="p-6 rounded-3xl flex space-x-3 dark:border dark:border-gray-800 hover:scale-105 duration-100 bg-[#1c1c2e]">
                   <Github />
                   <div>
                     <h4 class="dark:text-gray-300">View Of</h4>
